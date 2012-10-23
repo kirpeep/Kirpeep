1a8b56cba0dcea91b272edc5ac8aebc0f5647eec
John Lozano <John@kirpeep.com>
2012-10-23 14:05:25 -0500

    another push to test.kirpeep.com

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,7 +4,10 @@ TestApp::Application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+  # Code is not reloaded between requests
+  config.cache_classes = true
+  ThinkingSphinx.deltas_enabled = true 
+  ThinkingSphinx.updates_enabled = true
 
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
