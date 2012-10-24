61be6f41e3bdaeac708d81ff1051d6429d54bfe7
John Lozano <John@kirpeep.com>
2012-10-24 13:01:40 -0500

    Fixed issue with enter not working for signup

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -158,29 +158,4 @@ class ExchangesController < ApplicationController
     return ExchangeItem.where("exchange_id = ?", id).allfind { |e|  }
 
   end
-
-  #creates partial for additional offer listing when initiating an exchange
-  def add_offer
-    @targUser = User.find(params[:targ])
-    @initUser = User.find(params[:init])
-
-    render  :partial => 'create_offer', :locals => {:@targUser => @targUser, :@initUser => @initUser}
-  end
-
-  #creates partial for additional need listing when initiating an exchange
-  def add_need
-    @targUser = User.find(params[:targ])
-    @initUser = User.find(params[:init])
-
-    render  :partial => 'create_need', :locals => {:@targUser => @targUser, :@initUser => @initUser}
-  end
-
-  def link_to_add_fields(name, f, association)
-    new_object = f.object.send(association).klass.new
-    id = new_object.object_id
-    fields = f.fields_for(association, new_object, child_index: id) do |builder|
-      render("create_"+association.to_s.singularize , f: builder)
-    end
-    link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
-  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,13 +7,26 @@ module ApplicationHelper
 		link_to title, params.merge(:sort => column, :direction => direction, :page => nil), {:class => css_class}
 	end
 
-	def link_to_add_fields(name, f, association)
-		new_object = f.object.send(association).klass.new
-		id         = new_object.object_id
-		fields     = f.fields_for(association, new_object, child_index: id) do |builder|
-			render(association.to_s.singularize + "_fields", f: builder)
-    	end
-    	link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
-  end
+	def new_child_fields_template(form_builder, association, options = {})
+	  options[:object] ||= form_builder.object.class.reflect_on_association(association).klass.new
+	  options[:partial] ||= association.to_s.singularize
+	  options[:form_builder_local] ||= :f
+	  content_for :jstemplates do
+		content_tag(:div, :id => "#{association}_fields_template", :style => "display: none") do
+		  form_builder.fields_for(association, options[:object], :child_index => "new_#{association}") do |f|
+		    render(:partial => options[:partial], :locals => { options[:form_builder_local] => f })
+	      end
+	    end
+	  end
+	end
+
+	def add_offer_link(name, association)
+	  link_to(name, "javascript:void(0)", :class => "add_child", :"data-association" => association)
+	end
+
+	def add_need_link(name, association)
+	  link_to(name, "javascript:void(0)", :class => "add_child", :"data-association" => association)
+	end
+
 
 end
