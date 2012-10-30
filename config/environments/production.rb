bec331bac2029fe5526196729e4e11cdc06e92e9
kyleondata <kjennings.dev@gmail.com>
2012-10-30 15:19:46 -0600

    waaaawaaawaaaaa

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,10 @@ TestApp::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
+  config.after_initialize do
+    ActiveMerchant::Billing::Base.mode = :test
+  end
+
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
 
