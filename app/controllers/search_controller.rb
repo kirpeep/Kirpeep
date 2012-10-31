b7aee3148f62aebc0413b5ba13058efa5b25583f
John Lozano <John@kirpeep.com>
2012-10-31 16:16:14 -0500

    increased search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,9 +6,9 @@ class SearchController < ApplicationController
 	      @user = current_user
 	      @userlistings = UserListing.where("user_id != ? AND is_deleted == ?",@user.id.to_s, "false")#.paginate(:per_page => 5, :page => params[:page])#.order(sort_column + " " + sort_direction)
 	      #.paginate(:per_page => 5, :page => params[:page]) #.order(sort_column + " " + sort_direction)
-	      @userlistings = @userlistings.search(params[:search])
+	      @userlistings = @userlistings.search(params[:search], :per_page => 100)
 	    else
-	      @userlistings = UserListing.search(params[:search])#.paginate(:per_page => 5, :page => params[:page]) #.order(sort_column + " " + sort_direction)
+	      @userlistings = UserListing.search(params[:search], :per_page => 100)#.paginate(:per_page => 5, :page => params[:page]) #.order(sort_column + " " + sort_direction)
 	    end
     	render "show"
   	end
