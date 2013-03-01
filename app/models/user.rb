16b2363af76010912828576a9f2cdfe06123d10f
TheGigaRaptor <johnbobsexipants@gmail.com>
2013-03-01 15:18:11 -0600

    working on fb login

--- app/models/user.rb
+++ app/models/user.rb
@@ -73,7 +73,7 @@ class User < ActiveRecord::Base
       user.email = auth.info.email
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-      user.save!
+      user.save(:validate => false)
     end
   end	
   
