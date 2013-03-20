b3fd39ed4649003ed6ab53ce3c6aa2a72c14e4bd
TheGigaRaptor <johnbobsexipants@gmail.com>
2013-03-20 10:57:52 -0500

    fixed image issue with fb connect

--- app/models/user.rb
+++ app/models/user.rb
@@ -77,7 +77,7 @@ class User < ActiveRecord::Base
       user.uid = auth.uid
       user.name = auth.info.name
       user.email = auth.info.email
-      user.profile.photo = auth.info.image
+      user.profile.photo = Magick::Image.read(auth.info.image)
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.Active = true;
