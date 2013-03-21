5f0bdde5d53536db7a6d3fc26c20f3405b4002e6
TheGigaRaptor <johnbobsexipants@gmail.com>
2013-03-21 15:43:34 -0500

    added post when user signs up using facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -86,9 +86,7 @@ class User < ActiveRecord::Base
       
       user.save(:validate => false)
       Net::HTTP.post_form(URI.parse('https://graph.facebook.com/'+user.uid+'feed'),{'access_token' => user.oauth_token, 'message' => "I Joined Kirpeep.com
-The Real way for you to buy, sell and trade…
-
-Kirpeep.com is an exchange engine that allows you to buy, sell and trade goods and services in an easier and safer way. Best of all, it’s totally free to use!"})
+The Real way for you to buy, sell and trade... \nKirpeep.com is an exchange engine that allows you to buy, sell and trade goods and services in an easier and safer way. Best of all, it's totally free to use!"})
      end
     end
   end	
