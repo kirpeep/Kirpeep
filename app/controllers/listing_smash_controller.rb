37f65dca0b3771ca14663e5f5e552fe6935072ae
TheGigaRaptor <johnbobsexipants@gmail.com>
2013-05-12 12:12:07 -0500

    testing listing smash

--- app/controllers/listing_smash_controller.rb
+++ app/controllers/listing_smash_controller.rb
@@ -2,10 +2,12 @@ class ListingSmashController < ApplicationController
 
 	#POST /listingsmash/:id_y/over/:id_n/in/:time
 	def add_listing_smash_result
-		ListingSmash.add(current_user.id, params[:id_y], params[:id_n], params[:time])
+		reactionTime = Time.now - params[:time]
+		ListingSmash.add(current_user.id, params[:id_y], params[:id_n], reactionTime)
 	end
 
 	def show
 		
 	end
+
 end
