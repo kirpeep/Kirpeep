f5b9581db8e1c29a40422ff4457f6b223b61a020
TheGigaRaptor <johnbobsexipants@gmail.com>
2013-05-12 12:50:59 -0500

    fixed ListingSmash.rb

--- app/models/listing_smash.rb
+++ app/models/listing_smash.rb
@@ -2,11 +2,11 @@ class ListingSmash < ActiveRecord::Base
   attr_accessible :user_id, :listing_y, :listing_n, :reactiontime
     
   def self.add(userId, listing_y, listing_n, time)
-     @action = Action.new
-     @action.user_id = userId
-     @action.listing_y = listing_y
-     @action.listing_n = listing_n
-     @action.reactiontime = time
-     @action.save
+     @ls = ListingSmash.new
+     @ls.user_id = userId
+     @ls.listing_y = listing_y
+     @ls.listing_n = listing_n
+     @ls.reactiontime = time
+     @ls.save
   end
 end
